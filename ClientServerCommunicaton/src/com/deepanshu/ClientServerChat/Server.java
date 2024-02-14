6a87a0dc877c80ac82b36eb0a7234465b77526df
deep2555 <deepanshuprashar87@gmail.com>
2024-02-14 23:08:01 +0100

    have added the server class to connect and responsible for handling the client operations

--- ClientServerCommunicaton/src/com/deepanshu/ClientServerChat/Server.java
+++ ClientServerCommunicaton/src/com/deepanshu/ClientServerChat/Server.java
@@ -3,7 +3,8 @@ package com.deepanshu.ClientServerChat;
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Server code to handle multiple clients and communicate them*/
+		
 
 	}
 
